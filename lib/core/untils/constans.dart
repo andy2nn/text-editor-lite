ffae15144eedc8ad2205c732199c5e516d15bc0c
andyann <144227435+andy2nn@users.noreply.github.com>
2025-10-04 17:23:09 +0300

    константы актуальные

---
+++ lib/core/untils/constans.dart
@@ -0,0 +1,5 @@
+// константы для hive
+const String textsBox = 'textsBox';
+const String userBox = 'userBox';
+const String currentUser = 'currentUser';
+const String biometricEnabled = 'biometric_Eabled';
